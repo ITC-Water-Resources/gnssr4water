dd4d8f27c077e13bcf19b53248a7c3ff0bea365d
Roelof Rietbroek <r.rietbroek@utwente.nl>
2025-03-07 16:33:44 +0100

    fix import error of _version.py for editable installs

--- src/gnssr4water/__init__.py
+++ src/gnssr4water/__init__.py
@@ -1,1 +1,6 @@
-from gnssr4water._version import __version__,__version_tuple__
+try:
+    from gnssr4water._version import __version__,__version_tuple__
+except ImportError:
+    __version__ = "unknown"
+    __version_tuple__ = (0,0,0)
+
